59baa320ecca6ae62a3c01d69da241fa4aacc920
Khaled Al Jadaan <kjadaan@tecnoligia-unica.com.br>
2020-06-16 02:02:38 -0300

    Starting new project and adding Todos Controller.

---
+++ TodoList/Todo/Model/Todos.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Threading.Tasks;
+
+namespace Todo.Model
+{
+    public class Todos
+    {
+        public Todos()
+        {
+
+        }
+        public Todos(Guid Id, string Message)
+        {
+            this.Id = Id;
+            this.Message = Message;
+        }
+        public Guid Id { get; set; }
+
+        public string Message { get; set; }
+
+    }
+}
